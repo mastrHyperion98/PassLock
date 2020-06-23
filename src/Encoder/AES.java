1cc337a3e4f16d259339092ae7b4a7c806bbd862
Steven <s_smith1998@outlook.com>
2020-06-23 11:49:57 -0400

    Create AES class

---
+++ src/Encoder/AES.java
@@ -0,0 +1,11 @@
+package Encoder;
+
+/*
+Created by: Steven Smith
+Created on: 2020-06-23'
+Created for: PasswordManager project @ https://github.com/mastrHyperion98/PasswordManager
+
+Project under the GPL3 license. 
+ */
+public class AES {
+}
