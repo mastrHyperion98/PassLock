b6199d78b9d48fa7c030b3ac772e89cc0065a701
Steven <s_smith1998@outlook.com>
2020-07-08 20:15:54 -0400

    successfully change scene if password is correct

--- src/scenes/LoginController.java
+++ src/scenes/LoginController.java
@@ -2,19 +2,36 @@ package scenes;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     private static struct.Controller myController;
+    private static Stage main_stage;
     @FXML
     private Text actiontarget;
     @FXML private PasswordField passwordField;
 
-    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-        actiontarget.setText(""+myController.auth(passwordField.getText()));
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) throws IOException {
+        boolean valid = myController.auth(passwordField.getText());
+
+        if (valid){
+            Parent root = FXMLLoader.load(getClass().getResource("/scenes/first_time.fxml"));
+            Scene scene = new Scene(root, 400, 300);
+            main_stage.setScene(scene);
+        }
     }
     public static void setController(struct.Controller _controller){
         myController = _controller;
+    }
+
+    public static void setStage(Stage stage){
+        main_stage = stage;
     }
 }
