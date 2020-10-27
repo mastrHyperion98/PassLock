7b0772e0e45e03613803c543009a70634d484bd5
Steven <s_smith1998@outlook.com>
2020-10-27 02:03:29 -0400

    Read csv file and generate CSV object

--- src/main/java/com/mastrHyperion98/struct/CSV_Reader.java
+++ src/main/java/com/mastrHyperion98/struct/CSV_Reader.java
@@ -1,22 +1,45 @@
 package com.mastrHyperion98.struct;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class CSV_Reader {
-    public static void Write(File file) throws IOException {
-        FileReader fr = new FileReader(file);
-      /*  FileWriter fw = new FileWriter(file);
-        String[][] body = document.getBody();
-        String headers = String.join(",", document.getHeaders());
-        fw.write(headers + "\n");
-        for(int line = 0; line < document.getLinesCount()-1; line++){
-            String content = String.join(",", body[line]);
-            fw.write(content + "\n");
+    public static CSV Write(File file) throws IOException {
+        Scanner fr = new Scanner(file);
+        // get headers
+        if(!fr.hasNextLine())
+            return null;
+        String[] headers = fr.nextLine().split(",");
+        if(!validateFile(headers))
+            return null;
+        Queue<String[]> bodyLines = new LinkedList<>();
+
+        while(fr.hasNextLine()){
+            String[] line = fr.nextLine().split(",");
+            if(line.length == headers.length)
+                bodyLines.add(line);
         }
-        fw.flush();
-        fw.close();*/
+
+        String[][] body = new String[bodyLines.size()][headers.length];
+        int row = 0;
+        while(!bodyLines.isEmpty()){
+            String[] line = bodyLines.poll();
+            body[row] = line;
+        }
+
+        return new CSV(headers, body);
+    }
+
+    private static boolean validateFile(String[] headers){
+        boolean isValid = false;
+
+        if(headers.equals(new String[]{"domain", "username", "email", "password"})){
+           isValid = true;
+        }
+
+        return isValid;
     }
 }
