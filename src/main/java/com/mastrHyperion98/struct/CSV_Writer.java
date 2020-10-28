b67ab54e09f728b748342cfe94630cabf7edeb56
Steven <s_smith1998@outlook.com>
2020-10-27 22:42:29 -0400

    Cleanup CSV and CSV_Writer

--- src/main/java/com/mastrHyperion98/struct/CSV_Writer.java
+++ src/main/java/com/mastrHyperion98/struct/CSV_Writer.java
@@ -8,13 +8,7 @@ import java.io.IOException;
 public class CSV_Writer {
     public static void Write(File file, CSV document) throws IOException {
         FileWriter fw = new FileWriter(file);
-        String[][] body = document.getBody();
-        String headers = String.join(",", document.getHeaders());
-        fw.write(headers + "\n");
-        for(int line = 0; line < document.getLinesCount()-1; line++){
-            String content = String.join(",", body[line]);
-            fw.write(content + "\n");
-        }
+        fw.write(document.toString() + "\n");
         fw.flush();
         fw.close();
     }
